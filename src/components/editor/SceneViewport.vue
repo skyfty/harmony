52849e8dbcf040864f0cfa7d053dfdd5128e5dfd
skyfty <38198234+skyfty@users.noreply.github.com>
2025-10-17 00:53:54 +0800

    地

--- src/components/editor/SceneViewport.vue
+++ src/components/editor/SceneViewport.vue
@@ -42,7 +42,7 @@ const assetCacheStore = useAssetCacheStore()
 const isSceneReady = computed(() => sceneStore.isSceneReady)
 
 const tools: Array<{ label: string; icon: string; value: EditorTool, key: string }> = [
-  { label: 'Select', icon: 'mdi-cursor-default', value: 'select', key: 'KeyQ' },
+  { label: 'Select', icon: 'mdi-hand-back-right', value: 'select', key: 'KeyQ' },
   { label: 'Move', icon: 'mdi-axis-arrow', value: 'translate', key: 'KeyW' },
   { label: 'Rotate', icon: 'mdi-rotate-3d-variant', value: 'rotate', key: 'KeyE' },
   { label: 'Scale', icon: 'mdi-cube-scan', value: 'scale', key: 'KeyR' },
