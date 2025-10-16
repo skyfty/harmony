da6e989825dfabe8004e29d078c61597e8845b21
skyfty <38198234+skyfty@users.noreply.github.com>
2025-10-16 23:34:49 +0800

    sdf

--- src/components/editor/SceneViewport.vue
+++ src/components/editor/SceneViewport.vue
@@ -131,10 +131,10 @@ const draggingChangedHandler = (event: unknown) => {
   }
 }
 
-const gridHelper = new THREE.GridHelper(1000, 1000, 0x4dd0e1, 0x263238)
+const gridHelper = new THREE.GridHelper(1000, 1000, 0x4dd0e1, 0xFFFFFF)
 const gridMaterial = gridHelper.material as THREE.Material
 gridMaterial.depthWrite = false
-gridMaterial.opacity = 0.25
+gridMaterial.opacity = 0.35
 gridMaterial.transparent = true
 
 const axesHelper = new THREE.AxesHelper(4)
