c48e3f9cd25a8aaeecfeb0aad5c1e320d7fe392e
skyfty <38198234+skyfty@users.noreply.github.com>
2026-02-10 11:00:55 +0800

    枯

--- viewer/src/main.ts
+++ viewer/src/main.ts
@@ -2,10 +2,12 @@ import { createSSRApp } from "vue";
 import { createPinia } from 'pinia';
 import App from "./App.vue";
 
+
 export function createApp() {
   const app = createSSRApp(App);
   const pinia = createPinia();
   app.use(pinia);
+  console.log("App created");
   return {
     app,
     pinia,
