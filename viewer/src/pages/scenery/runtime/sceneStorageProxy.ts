3e4eeb5c6c4ac2cccca5f4abaee589c645ba1606
skyfty <38198234+skyfty@users.noreply.github.com>
2026-05-15 22:51:46 +0800

    sdf

--- viewer/src/pages/scenery/runtime/sceneStorageProxy.ts
+++
@@ -1,5 +0,0 @@
-export type { ScenePackagePointer } from '@harmony/utils/scene-package-storage';
-export {
-  removeScenePackageZip as discardScenePackageZip,
-  saveScenePackageZip as storeScenePackageZip,
-} from '@harmony/utils/scene-package-storage';
